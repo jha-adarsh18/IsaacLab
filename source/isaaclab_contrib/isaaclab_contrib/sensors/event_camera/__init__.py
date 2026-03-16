742840a5b508108e9620aae0c6ba7ca3f0ec7c94
Adarsh Jha <jhaadarsh350@gmail.com>
2026-03-16 22:33:39 +0530

    Adds event camera sensor directory scaffold

---
+++ source/isaaclab_contrib/isaaclab_contrib/sensors/event_camera/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Event camera sensor implementation for IsaacLab."""
+
+from .event_camera import EventCamera
+from .event_camera_cfg import EventCameraCfg
+from .event_camera_data import EventCameraData
